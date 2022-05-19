010aeac2d7b7e6f5beb4f17574e4fb0000075ec4
Lucille Njoo <lucillenjoo@gmail.com>
2022-05-19 01:37:04 -0700

    Starting on logistic regression

---
+++ logistic_regression.py
@@ -0,0 +1,13 @@
+from utils import read_talkdown
+
+
+condescending_set = read_talkdown()
+# read the clean set
+
+
+# load VAD lexicon
+# load concreteness lexicon
+# load LIWC
+
+
+# feature extraction
