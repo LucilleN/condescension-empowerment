acdad86750926066fe0294b7b1fb061c11b259c7
Lucille Njoo <lucillenjoo@gmail.com>
2022-06-04 20:24:00 -0700

    Can't get the 4th model (which includes LIWC and interactions) to converge even with 100 iterations, so we'll probably abandon that one

--- logistic_regression.py
+++ logistic_regression.py
@@ -72,6 +72,7 @@ def load_or_generate_data(condescending_set, empowering_set, power_scores, agenc
     return data
 
 if __name__ == "__main__":
+
     ### Read TalkDown data as condescending set
     condescending_set = read_talkdown()
     ### Read filtered Reddit scrape as empowering set
@@ -95,9 +96,20 @@ if __name__ == "__main__":
     data = load_or_generate_data(condescending_set, empowering_set, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category)
 
     lr_model_1 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness", data=data).fit()
+    
     lr_model_2 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness", data=data).fit()
-    # lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
+    
+    lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
+   
+    # This one fails to converge
     # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_count * social_count * relig_count * sexual_count * humans_count", data=data).fit()
+    # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_binary * social_binary * relig_binary * sexual_binary * humans_binary", data=data).fit()
+    
+    # Trying to exclude anger because it was not a significant predictor, as well as social_count and sexual_count because they were significant but had small beta's
+    # Still doesn't converge for all of the below
+    # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * relig_count * humans_count", data=data).fit()
+    # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * relig_count * humans_count", data=data).fit(maxiter=100)
+    # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * humans_count", data=data).fit(maxiter=100)
 
     print("\n\n######### MODEL 1: VAD, CONCRETENESS, NO INTERACTIONS #########\n")
     print(lr_model_1.summary())
@@ -105,8 +117,9 @@ if __name__ == "__main__":
     print("\n\n######### MODEL 2: VAD, CONCRETENESS, WITH INTERACTIONS #########\n")
     print(lr_model_2.summary())
 
-    # print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
-    # print(lr_model_3.summary())
+    print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC COUNTS, NO INTERACTIONS #########\n")
+    print(lr_model_3.summary())
 
-    # print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
+    # This one fails to converge
+    # print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC COUNTS, WITH INTERACTIONS #########\n")
     # print(lr_model_4.summary())
