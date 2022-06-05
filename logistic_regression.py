e3c028811e7a12bf54c5517ee83f13b98a158928
Lucille Njoo <lucillenjoo@gmail.com>
2022-06-04 20:08:49 -0700

    Organizing script into a main method

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,29 +11,7 @@ import pandas as pd
 from os.path import exists
 
 
-### Read TalkDown data as condescending set
-condescending_set = read_talkdown()
-### Read filtered Reddit scrape as empowering set
-empowering_set = read_filtered_reddit()
-
-
-### Load VAD lexicon
-sentiment_scores = read_VAD_scores("v") # v for valence
-agency_scores = read_VAD_scores("a") # a for agency
-power_scores = read_VAD_scores("d") # d for dominance
-
-
-### Load concreteness lexicon
-concreteness_scores = read_concreteness()
-
-### Load LIWC
-liwc_words_by_category = read_LIWC_lexicon()
-
-### Feature extraction
-X = [] # a list of lists, where rows are samples and columns are features
-y = [] # a list of 0's and 1's corresponding to the label of each sample. 0 = condescension, 1 = empowerment
-
-def get_feature_vector(sentence):
+def get_feature_vector(sentence, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category):
     # print("Extracting VAD features...")
     avg_power = get_sentence_lexicon_score(sentence, power_scores)
     avg_agency = get_sentence_lexicon_score(sentence, agency_scores)
@@ -63,18 +41,18 @@ def get_feature_vector(sentence):
     # print(feature_vector)
     return feature_vector
 
-def load_or_generate_data(condescending_set, empowering_set):
+def load_or_generate_data(condescending_set, empowering_set, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category):
     data = [] 
     if exists("data.pkl"):
         print("loading data...")
         data = pd.read_pickle("data.pkl")
     else:
         for sentence in condescending_set:
-            data_point = [0] + get_feature_vector(sentence)
+            data_point = [0] + get_feature_vector(sentence, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category)
             data.append(data_point)
 
         for sentence in empowering_set:
-            data_point = [1] + get_feature_vector(sentence)
+            data_point = [1] + get_feature_vector(sentence, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category)
             data.append(data_point)
 
         data = pd.DataFrame(data, columns = [
@@ -93,21 +71,42 @@ def load_or_generate_data(condescending_set, empowering_set):
         data.to_pickle("data.pkl")
     return data
 
-data = load_or_generate_data()
+if __name__ == "__main__":
+    ### Read TalkDown data as condescending set
+    condescending_set = read_talkdown()
+    ### Read filtered Reddit scrape as empowering set
+    empowering_set = read_filtered_reddit()
 
-lr_model_1 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness", data=data).fit()
-lr_model_2 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness", data=data).fit()
-# lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
-# lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_count * social_count * relig_count * sexual_count * humans_count", data=data).fit()
+    ### Load VAD lexicon
+    sentiment_scores = read_VAD_scores("v") # v for valence
+    agency_scores = read_VAD_scores("a") # a for agency
+    power_scores = read_VAD_scores("d") # d for dominance
 
-print("\n\n######### MODEL 1: VAD, CONCRETENESS, NO INTERACTIONS #########\n")
-print(lr_model_1.summary())
+    ### Load concreteness lexicon
+    concreteness_scores = read_concreteness()
 
-print("\n\n######### MODEL 2: VAD, CONCRETENESS, WITH INTERACTIONS #########\n")
-print(lr_model_2.summary())
+    ### Load LIWC
+    liwc_words_by_category = read_LIWC_lexicon()
 
-# print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
-# print(lr_model_3.summary())
+    ### Feature extraction
+    X = [] # a list of lists, where rows are samples and columns are features
+    y = [] # a list of 0's and 1's corresponding to the label of each sample. 0 = condescension, 1 = empowerment
 
-# print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
-# print(lr_model_4.summary())
+    data = load_or_generate_data(condescending_set, empowering_set, power_scores, agency_scores, sentiment_scores, concreteness_scores, liwc_words_by_category)
+
+    lr_model_1 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness", data=data).fit()
+    lr_model_2 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness", data=data).fit()
+    # lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
+    # lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_count * social_count * relig_count * sexual_count * humans_count", data=data).fit()
+
+    print("\n\n######### MODEL 1: VAD, CONCRETENESS, NO INTERACTIONS #########\n")
+    print(lr_model_1.summary())
+
+    print("\n\n######### MODEL 2: VAD, CONCRETENESS, WITH INTERACTIONS #########\n")
+    print(lr_model_2.summary())
+
+    # print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
+    # print(lr_model_3.summary())
+
+    # print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
+    # print(lr_model_4.summary())
