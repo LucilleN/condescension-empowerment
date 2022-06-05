49c3fb6e5a861ca0d9fa35b2bd0597e71efbc920
Lucille Njoo <lucillenjoo@gmail.com>
2022-06-04 20:04:25 -0700

    Saving data to file so we don't have to generate all of it every time

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,14 @@
-from utils import read_talkdown, read_filtered_reddit, read_VAD_scores, read_concreteness, get_sentence_lexicon_score, read_LIWC_lexicon, get_LIWC_count
+from utils import (
+    read_talkdown, 
+    read_filtered_reddit, 
+    read_VAD_scores, 
+    read_concreteness, 
+    get_sentence_lexicon_score, 
+    read_LIWC_lexicon, 
+    get_LIWC_count)
 import statsmodels.formula.api as smf
 import pandas as pd
+from os.path import exists
 
 
 ### Read TalkDown data as condescending set
@@ -55,32 +63,42 @@ def get_feature_vector(sentence):
     # print(feature_vector)
     return feature_vector
 
-data = []
+def load_or_generate_data(condescending_set, empowering_set):
+    data = [] 
+    if exists("data.pkl"):
+        print("loading data...")
+        data = pd.read_pickle("data.pkl")
+    else:
+        for sentence in condescending_set:
+            data_point = [0] + get_feature_vector(sentence)
+            data.append(data_point)
 
-for sentence in condescending_set:
-    data_point = [0] + get_feature_vector(sentence)
-    data.append(data_point)
+        for sentence in empowering_set:
+            data_point = [1] + get_feature_vector(sentence)
+            data.append(data_point)
 
-for sentence in empowering_set:
-    data_point = [1] + get_feature_vector(sentence)
-    data.append(data_point)
+        data = pd.DataFrame(data, columns = [
+            'is_empowering', # the label: 0 means condescending, 1 means empowering
+            'power', 
+            'agency',
+            'sentiment',
+            'concreteness',
+            "anger_count", "anger_binary", "anger_normalized",
+            "social_count", "social_binary", "social_normalized",
+            "relig_count", "relig_binary", "relig_normalized",
+            "sexual_count", "sexual_binary", "sexual_normalized",
+            "humans_count", "humans_binary", "humans_normalized"])
 
-data = pd.DataFrame(data, columns = [
-    'is_empowering', # the label: 0 means condescending, 1 means empowering
-    'power', 
-    'agency',
-    'sentiment',
-    'concreteness',
-    "anger_count", "anger_binary", "anger_normalized",
-    "social_count", "social_binary", "social_normalized",
-    "relig_count", "relig_binary", "relig_normalized",
-    "sexual_count", "sexual_binary", "sexual_normalized",
-    "humans_count", "humans_binary", "humans_normalized"])
+        print("saving data to file...")
+        data.to_pickle("data.pkl")
+    return data
+
+data = load_or_generate_data()
 
 lr_model_1 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness", data=data).fit()
 lr_model_2 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness", data=data).fit()
-lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
-lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_count * social_count * relig_count * sexual_count * humans_count", data=data).fit()
+# lr_model_3 = smf.logit("is_empowering ~ power + agency + sentiment + concreteness + anger_count + social_count + relig_count + sexual_count + humans_count", data=data).fit()
+# lr_model_4 = smf.logit("is_empowering ~ power * agency * sentiment * concreteness * anger_count * social_count * relig_count * sexual_count * humans_count", data=data).fit()
 
 print("\n\n######### MODEL 1: VAD, CONCRETENESS, NO INTERACTIONS #########\n")
 print(lr_model_1.summary())
@@ -88,8 +106,8 @@ print(lr_model_1.summary())
 print("\n\n######### MODEL 2: VAD, CONCRETENESS, WITH INTERACTIONS #########\n")
 print(lr_model_2.summary())
 
-print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
-print(lr_model_3.summary())
+# print("\n\n######### MODEL 3: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
+# print(lr_model_3.summary())
 
-print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
-print(lr_model_4.summary())
+# print("\n\n######### MODEL 4: VAD, CONCRETENESS, AND LIWC, NO INTERACTIONS #########\n")
+# print(lr_model_4.summary())
